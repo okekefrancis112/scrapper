8da9170e84d4033547f52b93a8d847decedea57a
okekefrancis112 <okeke98@gmail.com>
2023-12-07 13:50:53 +0100

    ...

---
+++ scrape.py
@@ -0,0 +1,70 @@
+import requests
+from bs4 import BeautifulSoup
+import sqlite3
+
+# Function to create a SQLite database and table
+def create_database():
+    conn = sqlite3.connect('store_data.db')
+    cursor = conn.cursor()
+
+    # Create a table to store item information
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS items (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            price REAL,
+            description TEXT
+        )
+    ''')
+
+    conn.commit()
+    conn.close()
+
+# Function to scrape data from the website and save it to the database
+def scrape_and_save_data():
+    url = 'https://example.com'  # Replace with the actual URL of the store website
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        items = soup.find_all('div', class_='item')  # Adjust the HTML structure based on the website
+
+        conn = sqlite3.connect('store_data.db')
+        cursor = conn.cursor()
+
+        for item in items:
+            name = item.find('span', class_='item-name').text.strip()
+            price = float(item.find('span', class_='item-price').text.strip().replace('$', ''))
+            description = item.find('p', class_='item-description').text.strip()
+
+            # Insert data into the database
+            cursor.execute('''
+                INSERT INTO items (name, price, description)
+                VALUES (?, ?, ?)
+            ''', (name, price, description))
+
+        conn.commit()
+        conn.close()
+        print('Data successfully scraped and saved to the database.')
+
+    else:
+        print('Failed to retrieve data from the website.')
+
+# Function to query data from the database
+def query_data():
+    conn = sqlite3.connect('store_data.db')
+    cursor = conn.cursor()
+
+    # Example query: retrieve all items with prices less than $50
+    cursor.execute('SELECT * FROM items WHERE price < 50')
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(f"ID: {row[0]}, Name: {row[1]}, Price: ${row[2]}, Description: {row[3]}")
+
+    conn.close()
+
+if __name__ == '__main__':
+    create_database()
+    scrape_and_save_data()
+    query_data()
